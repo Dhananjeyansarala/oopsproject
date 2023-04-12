ee3b5822d251c4710dc822ae7617dda32d94790f
Dhananjeyansarala <130529812+Dhananjeyansarala@users.noreply.github.com>
2023-04-12 10:17:21 +0530

    Update CubeOne.java

--- CubeOne.java
+++ CubeOne.java
@@ -3,7 +3,8 @@ package oops2test;
 public class CubeOne {
 	public void cubeOperation(int val1,int val2,int val3) {
 		int val=val1*val2*val3;
-		System.out.println(val);
+		System.out.println(multiply);
+	
 	}
 
 }
